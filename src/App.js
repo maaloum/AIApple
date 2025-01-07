c7afa1337774c40c4e747b900de444367cd420f5
maaloum <maaloumali1@gmail.com>
2025-01-07 16:51:48 -0500

    clean the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import FinancialTable from "./components/screens/FinancialTable";
 
 function App() {
   return (
     <>
       <Navbar />
+      <FinancialTable />
     </>
   );
 }
